346543499d382f759027a4655c9762cb1c11c37d
liadk07 <91607695+liad07@users.noreply.github.com>
2022-07-24 12:36:37 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
-    function f() {
+ function f() {
         var arr;
-
+        var  city=""
+        var  street=""
+        var  numhouse=""
+        var all=""
         var url="https://galasd4w3.pythonanywhere.com/?mikud="+ document.getElementById("mikud").value
         fetch(url)
             .then(response => response.json())
-            .then(data => arr=data)
+            .then(data =>document.getElementById("all").textContent=data.city+","+data.street+","+data.numhouse)
 /*
         arr.mikud//.replace("\r","")
         arr.city
         arr.street
         arr.numhouse
  */
-        console.log(arr)
-        document.getElementById("all").value=arr
-
+       // document.getElementById("all").value=
     }
