d1927265a17a8b3c7642a35c1112633c2096ea64
liadk07 <91607695+liad07@users.noreply.github.com>
2022-07-24 12:10:49 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+  function f() {
+        var arr;
+
+        var url="https://galasd4w3.pythonanywhere.com/?mikud="+ document.getElementById("mikud").value
+        fetch(url)
+            .then(response => response.json())
+            .then(data => arr=data)
+/*
+        arr.mikud//.replace("\r","")
+        arr.city
+        arr.street
+        arr.numhouse
+ */
+        console.log(arr.city+""+arr.street+""+arr.numhouse)
+       // document.getElementById("all").value=
+
+    }
